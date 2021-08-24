97cb96751dcf3f4e6265a0a60779076371b96158
Your Name <you@example.com>
2021-08-24 09:38:03 +0100

    test connection

---
+++ src/main/java/Exercise1/helloworld.java
@@ -0,0 +1,14 @@
+package Exercise1;
+
+
+public class helloworld {
+
+public static void main(String[] args) {
+	printMsg("HelloWorld!");
+	}
+
+	public static void printMsg(String message) {
+		System.out.println(message);
+	}
+
+}
